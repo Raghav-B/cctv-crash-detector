a8239dac92e4b8aca7320ec21f1a6f43f9f9d805
raghav-b <raghavbhardwaj@u.nus.edu>
2019-07-26 07:05:31 +0800

    Created GUI

--- demo/cctv.py
+++ demo/cctv.py
@@ -1,11 +1,63 @@
-import tkinter
+import numpy as np
 import cv2
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from detection_model import detection_model
+import datetime
 
-class App:
+class cctv:
     def __init__(self, window, window_title):
         self.window = window
         self.window.title(window_title)
+
+        self.dm = detection_model("../keras-retinanet/inference_graphs/resnet50_600p_51+/resnet50_csv_36.h5", \
+            src_video = "../videos/crash_all.mp4")
+
+        self.imageFrame = tk.Frame(self.window, width=600, height=600)
+        self.imageFrame.grid(row=0, column=0, padx=5, pady=5)
+
+        self.crash_images = ttk.Treeview(self.window, height=29)#, column=("#0"))#, height=600) #width=600)
+        self.crash_images.heading("#0", text="Detected Crashes")
+        self.crash_images.grid(row=0, column=600, padx=5, pady=5)
+        self.vsb = ttk.Scrollbar(self.window, orient="vertical", command=self.crash_images.yview)
+        self.vsb.place(x=812, y=8, height=605)
+        self.crash_images.configure(yscrollcommand=self.vsb.set)
+        self.crash_images.bind("<Double-1>", self.open_crash_image)
+        self.crash_dupl_checklist = set()
+
+        #Capture video frames
+        self.lmain = tk.Label(self.imageFrame)
+        self.lmain.grid(row=0, column=0)
+
+        self.update()
         self.window.mainloop()
 
-# Create a window and pass it to the Application object
-App(tkinter.Tk(), "CCTV")
+    def update(self):
+        ret, cv_image, frame_time, is_crash_detected = self.dm.get_frame()
+
+        if (ret == True):
+            frame = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGBA)
+            frame = Image.fromarray(frame)
+            frame = ImageTk.PhotoImage(image=frame)
+            self.lmain.imgtk = frame
+            self.lmain.configure(image=frame)
+
+            if (is_crash_detected == True):
+                current_time = datetime.datetime.now()
+                crash_name = current_time.strftime("%d-%m-%y_%H-%M-%S")# + str(current_time.microsecond)
+                if not (crash_name in self.crash_dupl_checklist):
+                    self.crash_dupl_checklist.add(crash_name)
+                    self.crash_images.insert("", 0, text=crash_name)
+                cv2.imwrite("crashes/" + crash_name + ".jpg", cv_image)
+
+        self.lmain.after(1, self.update)
+    
+    def open_crash_image(self, event):
+        query_image = self.crash_images.selection()[0]
+        open_image = cv2.imread("crashes/" + self.crash_images.item(query_image, "text") + \
+            ".jpg")
+        cv2.imshow("Detected Crash", open_image)
+
+
+cctv(tk.Tk(), "CCTV")
--- demo/detection_model.py
+++ demo/detection_model.py
@@ -13,143 +13,113 @@ from collections import deque
 from object_tracker import object_tracker
 
 class detection_model:
-    def __init__(self):
-        pass
+    def __init__(self, model_path, src_video=0, frame_skip=0):
+        keras.backend.tensorflow_backend.set_session(self.get_session())
+        self.ot = object_tracker()
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
 
-def get_session():
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    return tf.Session(config=config)
-keras.backend.tensorflow_backend.set_session(get_session())
+        
+        self.model = models.load_model(model_path, backbone_name='resnet50')
+        self.label_names = {0: "vehicle"}
+        
+        self.video = cv2.VideoCapture(src_video)
+        self.frame_width = self.video.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.frame_height = self.video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.frame_skip = frame_skip
+        self.total_framerate = 0
+        self.total_frames = 0
 
-model_path = "../keras-retinanet/inference_graphs/resnet50_600p_51+/resnet50_csv_36.h5"
-model = models.load_model(model_path, backbone_name='resnet50')
+        self.is_init_frame = True
+        self.prev_frame_objects = []
+        self.cur_frame_objects = []
 
-labels_to_names = {0: "vehicle"}
-
-ot = object_tracker()
-
-video = cv2.VideoCapture("../videos/crash_all.mp4")
-# Every 2 frames, I want to skip a frame.
-frame_skip_amt = 0 # This makes inferencing appear realtime
-frame_skip_start = 0
-
-is_init_frame = True
-font = cv2.FONT_HERSHEY_SIMPLEX
-prev_frame_objects = []
-cur_frame_objects = []
-
-total_framerate = 0
-total_frames = 0
-
-while True:
-    start_time = time.time()
-    # Performing frame-skip for increased performance
-    #if (frame_skip_start < 2):
-    #    frame_skip_start += 1
-    #else:
-    #    ret, frame = video.read()
-    #    frame_skip_start = 0
-
-    ret, frame = video.read()
-    if ret == False:
-        break
+    def get_session(self):
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        return tf.Session(config=config)
     
-    # Making input image square by padding it
-    rows = frame.shape[0]
-    cols = frame.shape[1]
-    if rows < cols:
-        padding = int((cols - rows) / 2)
-        frame = cv2.copyMakeBorder(frame, padding, padding, 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
-    elif rows > cols:
-        padding = int((rows - cols) / 2)
-        frame = cv2.copyMakeBorder(frame, 0, 0, padding, padding, cv2.BORDER_CONSTANT, (0, 0, 0))
-    
-    draw = frame.copy()
+    def get_frame(self):
+        start_time = time.time()
 
-    # preprocess image for network
-    image = preprocess_image(frame)
-    image, scale = resize_image(image, min_side=600)
-    # process image
-    boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
-    # correct for image scale
-    boxes /= scale
-    
-    # visualize detections
-    for box, score, label in zip(boxes[0], scores[0], labels[0]):
-        if score < 0.95:
-            break
+        for i in range(0, self.frame_skip):
+            ret, frame = self.video.read()
+        ret, frame = self.video.read()
+        if (ret == False):
+            return ret, None, -1, False 
 
-        midpoint_x = int((box[2] + box[0]) / 2)
-        midpoint_y = int((box[3] + box[1]) / 2)
+        # Making input image square by padding it
+        rows = frame.shape[0]
+        cols = frame.shape[1]
+        if rows < cols:
+            padding = int((cols - rows) / 2)
+            frame = cv2.copyMakeBorder(frame, padding, padding, 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
+        elif rows > cols:
+            padding = int((rows - cols) / 2)
+            frame = cv2.copyMakeBorder(frame, 0, 0, padding, padding, cv2.BORDER_CONSTANT, (0, 0, 0))
+        
+        # Making copy of raw frame for drawing detections and other info
+        draw_frame = frame.copy()
 
-        if (is_init_frame == True):
-            prev_frame_objects.append([(midpoint_x, midpoint_y), ot.get_init_index(), 0, deque(), -1, 0])
-        else:
-            # All objects detected in current frame are initialised with index -1, after running through
-            # the object sorter, objects are assigned appropriate indexes.
-            cur_frame_objects.append([(midpoint_x, midpoint_y), 0, 0, deque(), -1, 0])
+        frame = preprocess_image(frame)
+        frame, scale = resize_image(frame, min_side=600)
+        boxes, scores, labels = self.model.predict_on_batch(np.expand_dims(frame, axis=0))
+        boxes /= scale
 
-        #b = box.astype(int)
-        #draw_box(draw, b, color=(0, 255, 255))
-        #caption = "{} {:.2f}".format(labels_to_names[label], score)
-        #draw_caption(draw, b, caption)
-    
-    # Sorting cur_frame midpoints
-    if (is_init_frame == False):
-        if (len(prev_frame_objects) != 0):
-            cur_frame_objects = ot.sort_cur_objects(prev_frame_objects, cur_frame_objects)
-    
-    end_time = time.time() - start_time
-    #print("Framerate: " + str(int(1/end_time)))
-    total_framerate += (1/end_time)
-    total_frames += 1
+        # Filter detections and prepare for object tracking
+        for box, score, label in zip(boxes[0], scores[0], labels[0]):
+            if score < 0.95:
+                break
 
-    is_crash_detected = False
-    for point in cur_frame_objects:
-        # Only drawing index for object that has been detected for 3 frames
-        if (point[2] >= 5):            
-            # Finding vector of car and converting to unit vector
-            vector = [point[3][-1][0] - point[3][0][0], point[3][-1][1] - point[3][0][1]] # [x, y]
-            vector_mag = (vector[0]**2 + vector[1]**2)**(1/2)
-            
-            dist = abs(vector_mag - point[5])
+            midpoint_x = int((box[2] + box[0]) / 2)
+            midpoint_y = int((box[3] + box[1]) / 2)
 
-            has_object_crashed = False
-            colour = (0, 255, 255)
-            if (dist >= 11) and point[5] != 0.0:
-                colour = (0, 0, 255)
-                is_crash_detected = True
-                has_object_crashed = True
-            
-            # Showing both magnitude and direction of vector
-            end_point = (2 * vector[0] + point[3][-1][0], 2 * vector[1] + point[3][-1][1]) # (x, y)
-            # Showing index of object on object
-            #cv2.putText(draw, f"{int(point[1])}", point[0], font, 1, (0, 255, 255), 2, cv2.LINE_AA)
-            #cv2.putText(draw, f"{dist:.1f},{vector_mag:.1f},{point[5]:.1f}", point[0], font, 0.5, colour, 2, cv2.LINE_AA)
-            cv2.circle(draw, point[0], 5, (0, 255, 255), 2)
-            if (has_object_crashed == True):
-                cv2.circle(draw, point[0], 40, (0, 0, 255), 4)
+            if (self.is_init_frame == True):
+                self.prev_frame_objects.append([(midpoint_x, midpoint_y), self.ot.get_init_index(), 0, deque(), -1, 0])
+            else:
+                self.cur_frame_objects.append([(midpoint_x, midpoint_y), 0, 0, deque(), -1, 0])
 
-            cv2.line(draw, point[3][-1], end_point, (255, 255, 0), 2)
-    
-    if (is_crash_detected == True):
-        cv2.putText(draw, f"CRASH DETECTED", (10, 30), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
-        cv2.imshow("Crash", draw)
+        # Running object tracking algorithm to track detections across frames
+        if (self.is_init_frame == False):
+            if (len(self.prev_frame_objects) != 0):
+                self.cur_frame_objects = self.ot.sort_cur_objects(self.prev_frame_objects, self.cur_frame_objects)
 
-    if (is_init_frame == False):
-        prev_frame_objects = cur_frame_objects.copy()
-        cur_frame_objects = []
+        frame_time = time.time() - start_time
+        #print("Framerate: " + str(int(1/frame_time)))
+        self.total_framerate += (1/frame_time)
+        self.total_frames += 1
 
-    cv2.imshow("CCTV", draw)
+        is_crash_detected = False
+        for point in self.cur_frame_objects:
+            # Only drawing index for object that has been detected for 3 frames
+            if (point[2] >= 5):            
+                # Finding vector of car and converting to unit vector
+                vector = [point[3][-1][0] - point[3][0][0], point[3][-1][1] - point[3][0][1]] # [x, y]
+                end_point = (2 * vector[0] + point[3][-1][0], 2 * vector[1] + point[3][-1][1]) # (x, y)
+                vector_mag = (vector[0]**2 + vector[1]**2)**(1/2)
+                delta = abs(vector_mag - point[5])
 
-    is_init_frame = False
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
-    elif key == ord('p'):
-        cv2.waitKey()
+                has_object_crashed = False
+                if (delta >= 11) and point[5] != 0.0:
+                    is_crash_detected = True
+                    has_object_crashed = True
+                
+                cv2.circle(draw_frame, point[0], 5, (0, 255, 255), 2)
+                if (has_object_crashed == True):
+                    cv2.circle(draw_frame, point[0], 40, (0, 0, 255), 4)
 
-print(f"Average FPS: {(total_framerate / total_frames):.2f}")
-video.release()
-cv2.destroyAllWindows()
+                # Drawing vector of each object
+                cv2.line(draw_frame, point[3][-1], end_point, (255, 255, 0), 2)
+
+        if (is_crash_detected == True):
+            cv2.putText(draw_frame, f"CRASH DETECTED", (10, 30), self.font, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            #cv2.imshow("Crash", draw_frame)
+
+        if (self.is_init_frame == False):
+            self.prev_frame_objects = self.cur_frame_objects.copy()
+            self.cur_frame_objects = []
+        
+        self.is_init_frame = False
+        return ret, draw_frame, frame_time, is_crash_detected
+        
+    def __del__(self):
+        self.video.release()
